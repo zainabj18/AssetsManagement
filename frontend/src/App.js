37b1cb45f6e7c1c3b3769aa211c02e9e04ae6a25
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-17 18:49:04 +0000

    Add a NoMatch route when URL not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './routes/Layout';
+import NoMatch from './routes/NoMatch';
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Layout />} />
+			<Route path="/" element={<Layout />}>
+				<Route path="*" element={<NoMatch />} />
+			</Route>
 		</Routes>
 	);
 }
---
+++ frontend/src/routes/NoMatch.js
@@ -0,0 +1,9 @@
+const NoMatch = () => {
+	return (
+		<div>
+			<p>Sorry Page Not Found!</p>
+		</div>
+	);
+};
+
+export default NoMatch;
