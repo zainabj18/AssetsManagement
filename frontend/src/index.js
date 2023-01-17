e8bcfcb221c14e08625c96bae6fa2200e6a0cf29
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-17 19:08:35 +0000

    Setup custom theme for chakra-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import customTheme from './theme/index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<ChakraProvider>
+			<ChakraProvider theme={customTheme}>
 				<App />
 			</ChakraProvider>
 		</BrowserRouter>
---
+++ frontend/src/theme/index.js
@@ -0,0 +1,10 @@
+// theme/index.js
+import { extendTheme } from '@chakra-ui/react';
+
+import styles from './styles';
+
+const overrides = {
+	styles,
+};
+
+export default extendTheme(overrides);
