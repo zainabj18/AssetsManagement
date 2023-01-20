cf9d69ef0e282bc48f52f7018b5ebb9b8d909e2a
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-20 01:06:11 +0000

    Enabled assset viwer to read basic json data

--- frontend/src/routes/AssetVeiwer.js
+++ frontend/src/routes/AssetVeiwer.js
@@ -1,23 +1,73 @@
-import { Container } from '@chakra-ui/react';
+import {
+	Container,
+	Stat,
+	StatLabel,
+	StatNumber,
+	StatGroup,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import FormField from '../components/FormField';
-
+import data from './../api.json';
 const AssetViewer = () => {
+	const { id } = useParams();
+	const [assetSate, setAssetSate] = useState(null);
+	const [isDisabled, setIsDisabled] = useState(true);
+	const [startWithEditView, setStartWithEditView] = useState(true);
 	const handleChange = (e) => {
 		console.log(e);
 	};
 
-	return (
+	useEffect(() => {
+		setAssetSate(data[id]);
+	}, [id]);
+
+	return assetSate ? (
 		<Container>
+			<StatGroup>
+				<Stat>
+					<StatLabel>Created At</StatLabel>
+					<StatNumber>{assetSate.created_at}</StatNumber>
+				</Stat>
+				<Stat>
+					<StatLabel>Last Modified</StatLabel>
+					<StatNumber>{assetSate.last_modified_at}</StatNumber>
+				</Stat>
+			</StatGroup>
 			<FormField
 				fieldName="name"
 				fieldType="text"
-				fieldDefaultValue="name"
-				isDisabled={false}
-				startWithEditView={true}
+				fieldDefaultValue={assetSate.name}
+				isDisabled={isDisabled}
+				startWithEditView={startWithEditView}
+				onSubmitHandler={handleChange}
+			/>
+			<FormField
+				fieldName="link"
+				fieldType="url"
+				fieldDefaultValue={assetSate.link}
+				isDisabled={isDisabled}
+				startWithEditView={startWithEditView}
+				onSubmitHandler={handleChange}
+			/>
+			<FormField
+				fieldName="type"
+				fieldType="text"
+				fieldDefaultValue={assetSate.type}
+				isDisabled={isDisabled}
+				startWithEditView={startWithEditView}
+				onSubmitHandler={handleChange}
+			/>
+			<FormField
+				fieldName="description"
+				fieldType="text"
+				fieldDefaultValue={assetSate.description}
+				isDisabled={isDisabled}
+				startWithEditView={startWithEditView}
 				onSubmitHandler={handleChange}
 			/>
 		</Container>
-	);
+	) : null;
 };
 
 export default AssetViewer;
