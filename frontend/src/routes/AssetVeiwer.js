cd8da8b51c633c8725cfa8607ba6af93cc8a7d80
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-20 08:11:27 +0000

    Added ablity to render metadata of an asset

--- frontend/src/routes/AssetVeiwer.js
+++ frontend/src/routes/AssetVeiwer.js
@@ -4,7 +4,11 @@ import {
 	StatLabel,
 	StatNumber,
 	StatGroup,
+	Divider,
+	VStack,
+	Heading,
 } from '@chakra-ui/react';
+import { Fragment } from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FormField from '../components/FormField';
@@ -27,48 +31,76 @@ const AssetViewer = () => {
 
 	return assetSate ? (
 		<Container>
-			<StatGroup>
-				<Stat>
-					<StatLabel>Created At</StatLabel>
-					<StatNumber>{assetSate.created_at}</StatNumber>
-				</Stat>
-				<Stat>
-					<StatLabel>Last Modified</StatLabel>
-					<StatNumber>{assetSate.last_modified_at}</StatNumber>
-				</Stat>
-			</StatGroup>
-			<FormField
-				fieldName="name"
-				fieldType="text"
-				fieldDefaultValue={assetSate.name}
-				isDisabled={isDisabled}
-				startWithEditView={startWithEditView}
-				onSubmitHandler={handleChange}
-			/>
-			<FormField
-				fieldName="link"
-				fieldType="url"
-				fieldDefaultValue={assetSate.link}
-				isDisabled={isDisabled}
-				startWithEditView={startWithEditView}
-				onSubmitHandler={handleChange}
-			/>
-			<FormField
-				fieldName="type"
-				fieldType="text"
-				fieldDefaultValue={assetSate.type}
-				isDisabled={isDisabled}
-				startWithEditView={startWithEditView}
-				onSubmitHandler={handleChange}
-			/>
-			<FormField
-				fieldName="description"
-				fieldType="text"
-				fieldDefaultValue={assetSate.description}
-				isDisabled={isDisabled}
-				startWithEditView={startWithEditView}
-				onSubmitHandler={handleChange}
-			/>
+			<VStack>
+				<Heading size={'2xl'}>Asset Attributes</Heading>
+				<StatGroup>
+					<Stat>
+						<StatLabel>Created At</StatLabel>
+						<StatNumber>{assetSate.created_at}</StatNumber>
+					</Stat>
+					<Stat>
+						<StatLabel>Last Modified</StatLabel>
+						<StatNumber>{assetSate.last_modified_at}</StatNumber>
+					</Stat>
+				</StatGroup>
+				<FormField
+					fieldName="name"
+					fieldType="text"
+					fieldDefaultValue={assetSate.name}
+					isDisabled={isDisabled}
+					startWithEditView={startWithEditView}
+					onSubmitHandler={handleChange}
+				/>
+				<FormField
+					fieldName="link"
+					fieldType="url"
+					fieldDefaultValue={assetSate.link}
+					isDisabled={isDisabled}
+					startWithEditView={startWithEditView}
+					onSubmitHandler={handleChange}
+				/>
+				<FormField
+					fieldName="type"
+					fieldType="text"
+					fieldDefaultValue={assetSate.type}
+					isDisabled={isDisabled}
+					startWithEditView={startWithEditView}
+					onSubmitHandler={handleChange}
+				/>
+				<FormField
+					fieldName="description"
+					fieldType="text"
+					fieldDefaultValue={assetSate.description}
+					isDisabled={isDisabled}
+					startWithEditView={startWithEditView}
+					onSubmitHandler={handleChange}
+				/>
+				<FormField
+					fieldName="ds"
+					fieldType="checkbox"
+					fieldDefaultValue={true}
+					isDisabled={isDisabled}
+					startWithEditView={startWithEditView}
+					onSubmitHandler={handleChange}
+				/>
+				<Divider />
+				<Heading size={'md'}>Type Attributes:</Heading>
+
+				{assetSate.metadata.map((value, key) => {
+					return (
+						<Fragment key={key}>
+							<FormField
+								fieldName={value.attributeName}
+								fieldType={value.attributeType}
+								fieldDefaultValue={value.attributeValue}
+								isDisabled={isDisabled}
+								startWithEditView={startWithEditView}
+								onSubmitHandler={handleChange}
+							/>
+						</Fragment>
+					);
+				})}
+			</VStack>
 		</Container>
 	) : null;
 };
