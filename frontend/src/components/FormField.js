67118d93c8b1c013e71f8f5765b0adf878ee02c5
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-20 00:07:12 +0000

    Added FormField component for editable input.

---
+++ frontend/src/components/FormField.js
@@ -0,0 +1,38 @@
+import {
+	FormControl,
+	FormLabel,
+	Editable,
+	EditablePreview,
+	EditableInput,
+	Input,
+} from '@chakra-ui/react';
+import EditableControls from './EditableControls';
+const FormField = ({
+	fieldName,
+	fieldType,
+	fieldDefaultValue,
+	isDisabled,
+	startWithEditView,
+	onSubmitHandler,
+}) => {
+	return (
+		<FormControl bg="white" color="black">
+			<FormLabel>{fieldName}</FormLabel>
+			<Editable
+				textAlign="center"
+				defaultValue={fieldDefaultValue}
+				startWithEditView={startWithEditView}
+				isDisabled={isDisabled}
+				onSubmit={(e) => {
+					onSubmitHandler(e);
+				}}
+			>
+				<EditablePreview />
+				<Input type={fieldType} as={EditableInput} />
+				<EditableControls />
+			</Editable>
+		</FormControl>
+	);
+};
+
+export default FormField;
