2e3a6d8d4cb23d10a20fee6b87ce682c3f442c10
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-22 05:45:25 +0000

    Added test to ensure that register route has enough into to create a user

--- backend/tests/integration/test_auth.py
+++ backend/tests/integration/test_auth.py
@@ -1,3 +1,38 @@
 def test_register_requires_username(client):
-    res=client.post("/api/v1/auth/register",json={"username":"user"})
+    res=client.post("/api/v1/auth/register",json={"password":"user","confirm_password":"user"}
+)
     assert res.status_code==400
+    assert res.json==[
+    {
+        "loc": [
+            "username"
+        ],
+        "msg": "field required",
+        "type": "value_error.missing"
+    }]
+
+def test_register_requires_password(client):
+    res=client.post("/api/v1/auth/register",json={"username":"user","confirm_password":"user"}
+)
+    assert res.status_code==400
+    assert res.json==[
+    {
+        "loc": [
+            "password"
+        ],
+        "msg": "field required",
+        "type": "value_error.missing"
+    }]
+
+def test_register_requires_confrim_password(client):
+    res=client.post("/api/v1/auth/register",json={"username":"user","password":"user"}
+)
+    assert res.status_code==400
+    assert res.json==[
+    {
+        "loc": [
+            "confirmPassword"
+        ],
+        "msg": "field required",
+        "type": "value_error.missing"
+    }]
