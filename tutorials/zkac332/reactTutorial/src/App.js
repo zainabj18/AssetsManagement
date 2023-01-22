74d94d55a9de5f794ba281f33309b6f23d37ac0b
Higashiuezato Takase (2021) ZKAC332 <takase.higashiuezato.2021@live.rhul.ac.uk>
2023-01-22 23:08:44 +0000

    Add react tutorial: it hasn't been had any connection with back-end server yet

---
+++ tutorials/zkac332/reactTutorial/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <>
+    <TodoList />
+    <div>Username</div>
+    <input type="text" />
+    <div>Password</div>
+    <input type="text" />
+    <button>Login</button>
+    </>
+    
+  )
+}
+
+export default App;
