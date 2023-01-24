c17573bec40ab9d4a5c6037d8a1610a1d3a812ff
Dmani Barnett <dmani.barnett@gmail.com>
2023-01-24 16:15:54 +0000

    Added new user route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './routes/Layout';
 import NoMatch from './routes/NoMatch';
+import User from './routes/User';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
+				<Route path="newuser" element={<User />} />
 				<Route path="*" element={<NoMatch />} />
 			</Route>
 		</Routes>
---
+++ frontend/src/routes/User.js
@@ -0,0 +1,6 @@
+import { VStack } from '@chakra-ui/react';
+const User = () => {
+	return <VStack></VStack>;
+};
+
+export default User;
