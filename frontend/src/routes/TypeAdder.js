4db035606af7e4f71e33931e1e2b4ae68a354901
Zuhayr <zuhayrmohsen@gmail.com>
2023-01-25 21:30:05 +0000

    Added UI design for type adder

--- frontend/src/routes/TypeAdder.js
+++ frontend/src/routes/TypeAdder.js
@@ -1,11 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import { Heading, VStack,Text } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react';
+import {
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	FormHelperText,
+  } from '@chakra-ui/react'
+
 
 const TypeAdder = () => {
 	return (
 		<VStack minW="100vw">
 			<Text>TypeAdder</Text>
+			<FormControl>
+  				<FormLabel>Name</FormLabel>
+  				<Input type='text' placeholder = 'Name'/>
+			</FormControl>
+			<FormControl>
+  				<FormLabel>Attribute Name</FormLabel>
+  				<Input type='text' placeholder = 'AttributeName'/>
+			</FormControl>
+			<FormControl>
+  				<FormLabel>Data Type</FormLabel>
+  				<Input type='text' placeholder = 'DataType'/>
+			</FormControl>
 		</VStack>
 	);
 };
