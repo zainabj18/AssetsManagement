1679fc60f53082db99cffbf60dad66088d22bf1a
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-01-25 21:07:12 +0000

    Added a new path and blueprint for the types. Added the path to the front end.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './routes/Layout';
 import NoMatch from './routes/NoMatch';
+import TypeAdder from './routes/TypeAdder';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route path="*" element={<NoMatch />} />
+				<Route path="/type/adder" element={<TypeAdder />} />
 			</Route>
 		</Routes>
 	);
---
+++ frontend/src/routes/TypeAdder.js
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { Heading, VStack,Text } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+
+const TypeAdder = () => {
+	return (
+		<VStack minW="100vw">
+			<Text>TypeAdder</Text>
+		</VStack>
+	);
+};
+
+export default TypeAdder;
