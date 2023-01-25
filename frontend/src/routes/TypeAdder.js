bea66ac664c3cc67944d9b9c95c78e2b0f642f0d
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-01-25 22:04:07 +0000

    Added the ability to add attributes dynamicly

--- frontend/src/routes/TypeAdder.js
+++ frontend/src/routes/TypeAdder.js
@@ -17,8 +17,11 @@ const TypeAdder = () => {
 	const [inputFields, setInputFields] = useState([
 		{ attrName: 'programming language', attrType: 'text' },
 		{ attrName: 'country of origin', attrType: 'text' }
-
 	  ])
+	const addAttribute = () => {
+		let newAttribute = { attrName: '', attrType: '' }
+		setInputFields([...inputFields, newAttribute])
+	}
 	return (
 		<VStack minW="100vw">
 			<Text>TypeAdder</Text>
@@ -38,7 +41,7 @@ const TypeAdder = () => {
 				</FormControl>
 			</HStack>)})}
 			
-			
+			<Button colorScheme='blue' onClick={addAttribute}>Add</Button>
 			<Button colorScheme='blue'>Save</Button>
 		</VStack>
 	);
