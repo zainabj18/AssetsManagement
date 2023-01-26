9e9dfe4b7fa9205cc1f4a8f629aee3aa987824f6
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2023-01-26 11:30:11 +0000

    created a search bar on a new webpage. Made a get request to fetch data from the data.js file that will be temporarily used to search

--- frontend/src/routes/KeywordSearch.js
+++ frontend/src/routes/KeywordSearch.js
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Data from './Data.js';
-
+import { Card, Input } from 'semantic-ui-react';
+import { filter } from '@chakra-ui/react';
 
 export default function KeywordSearch() {
-    const[query, setQuery] = useState('');
+    const[APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        //get request to fetch data 
+        axios.get('src/routes/Data.js')
+        .then((response) => {
+            setAPIData(response.data);
+        });
+    }, []);
 
 
+    //creating a search bar on he webpage
     return (
-        <div className='KeywordSearch'>
-            <label>Search</label>
-            <input type='text' onChange={(e) => setQuery (e.target.value)} />
+        <div style={{ padding: 20}}>
+            <Input icon='search'
+                placeholder='Search'
+            />
         </div>
     );
 }
