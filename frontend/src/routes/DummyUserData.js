5478dd846a80794c18a74c1bffb6a74e61ad5227
Zayn Rashid <rangedbear@gmail.com>
2023-01-26 10:01:02 +0000

    Created new file that will store the dummy data that will test the account creation.

---
+++ frontend/src/routes/DummyUserData.js
@@ -0,0 +1,9 @@
+[
+    {
+        "username": "user",
+        "password": "chodIpRaf2udrif",
+        "confirmPassword": "chodIpRaf2udrif",
+        "accountPrivileges": "PUBLIC",
+        "accountType": "VIEWER"
+    }
+]
