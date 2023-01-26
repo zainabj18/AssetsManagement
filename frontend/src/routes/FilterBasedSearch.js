614246bf0b9311af9c2dfb7bdb089927c0f1ca67
Suman0912 <zkac181@live.rhul.ac.uk>
2023-01-26 12:10:04 +0000

    Added some high-level elements that can expand/collapse with Accordion

--- frontend/src/routes/FilterBasedSearch.js
+++ frontend/src/routes/FilterBasedSearch.js
@@ -1,6 +1,47 @@
 import { VStack } from '@chakra-ui/react';
-const FilterBasedSearch = () => {
-	return <VStack> <p>This is the filter based search page !!</p></VStack>;
-};
+import { Box } from '@chakra-ui/react';
+import {Accordion,AccordionItem,AccordionButton,AccordionPanel,AccordionIcon} from '@chakra-ui/react';
 
+const FilterBasedSearch = () => {
+	return (
+		<Box p={30}>
+			<VStack> <p>This is the Filter Based Search Page !!</p></VStack>
+			<Accordion defaultIndex={[0]} allowMultiple>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>Asset Type
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+						...
+					</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							Tags
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+						...
+					</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							Projects
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+						...
+					</AccordionPanel>
+				</AccordionItem>
+			</Accordion>
+		</Box>
+	);
+  
+};
 export default FilterBasedSearch;
