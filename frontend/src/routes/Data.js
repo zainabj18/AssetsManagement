d234303f69fa4df729275fa8b69614d2e95638a0
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2023-01-26 11:33:24 +0000

    double paranthesis were creating errors so changed them to single paranthesis to get the code to compile successfully

--- frontend/src/routes/Data.js
+++ frontend/src/routes/Data.js
@@ -1,38 +1,38 @@
 [
     {
-        "asset": "Authenticating",
-        "language": "Java",
-        "data": "class"
+        'asset': 'Authenticating',
+        'language': 'Java',
+        'data': 'class'
 
     },
 
     {
-        "asset": "Merged",
-        "language": "Java",
-        "data": "class"
+        'asset': 'Merged',
+        'language': 'Java',
+        'data': 'class'
     },
 
     {
-        "asset": "Wrapped",
-        "language": "Java",
-        "data": "class"
+        'asset': 'Wrapped',
+        'language': 'Java',
+        'data': 'class'
     },
 
     {
-        "asset": "Auth",
-        "language": "Java",
-        "data": "class"
+        'asset': 'Auth',
+        'language': 'Java',
+        'data': 'class'
     },
 
     {
-        "asset": "Auth",
-        "language": "Python",
-        "data": "class"
+        'asset': 'Auth',
+        'language': 'Python',
+        'data': 'class'
     },
 
     {
-        "asset": "Merged",
-        "language": "Python",
-        "data": "class"
+        'asset': 'Merged',
+        'language': 'Python',
+        'data': 'class'
     }
 ]
