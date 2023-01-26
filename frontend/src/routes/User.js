1b15427fa99d9e4613bef99c703c8127aeb0c8d3
Zayn Rashid <rangedbear@gmail.com>
2023-01-26 09:42:52 +0000

    Implemented select button for Role that will  extend and show the multiple role choices when clicked.

--- frontend/src/routes/User.js
+++ frontend/src/routes/User.js
@@ -37,10 +37,20 @@ const User = () => {
 				<Box mr={280}>
 					Access Level
 				</Box>
+				<Box mr={210}>
+					Role
+				</Box>
 			</Flex>
 			<HStack>
 				<Box>
-					<Select placeholder='Select option' color='black' bg='white' right={6}>
+					<Select placeholder='Select option' color='black' bg='white' right={7}>
+						<option value='option1'>Option 1</option>
+						<option value='option2'>Option 2</option>
+						<option value='option3'>Option 3</option>
+					</Select>
+				</Box>
+				<Box>
+					<Select placeholder='Select option' color='black' bg='white' left={162}>
 						<option value='option1'>Option 1</option>
 						<option value='option2'>Option 2</option>
 						<option value='option3'>Option 3</option>
