a7495c0ab5c6bc5467b977399e2eb0c302f36498
Higashiuezato Takase (2021) ZKAC332 <takase.higashiuezato.2021@live.rhul.ac.uk>
2023-01-26 12:24:29 +0000

    Implemented functions using Chakra UI library.

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios'; //for HTTP requests
+import { FormControl, FormLabel, Input, Button, useColorMode } from '@chakra-ui/react';
 
 function Login() {
 
@@ -7,16 +7,16 @@ function Login() {
 	const [password, setPassword] = useState('');
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	const [error, setError] = useState('');
-  
+	const {colorMode, toggleColorMode } = useColorMode();
+	const [show, setShow] = React.useState(false);
+	const handleClick = () => setShow(!show);
+
+    
 	const handleLogin = () => {
 		try {
-			console.log('I am hee');
-			// const res = axios.post('https://localhost:5000/api/v1/auth/login', { //await keyword to send a POST request to /login endpoint
-			// 	username,
-			// 	password
-			// });
+			console.log('I am here');
 			fetch('https://localhost:5000/api/v1/auth/login',{
-				methods: 'POST',
+				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 					'Access-Control-Allow-Origin': 'true'
@@ -43,27 +43,34 @@ function Login() {
 
 	return (
 		<div>
+			<div style={{ position: 'absolute', top: '0', right: '0'}}>
+				<button style={{ border: '1px solid #ccc'}} onClick={toggleColorMode}> {colorMode === 'light' ?  'Dark' : 'Light'} </button>
+			</div>
 			{isAuthenticated ? (
 				<div>Welcome!</div>
 			) : (
 				<form>
-					<label htmlFor="username">Username</label>
-					<input
-						type="text"
-						id="username"
-						value={username}
-						onChange={e => setUsername(e.target.value)}
-					/>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						value={password}
-						onChange={e => setPassword(e.target.value)}
-					/>
-					<button onClick={handleLogin}>
-          Login
-					</button>
+					<FormControl>
+						<FormLabel htmlFor='username'>Username</FormLabel>
+						<Input type='text' id='username' value={username} onChange={e => setUsername(e.target.value)}/>
+					</FormControl>
+					<FormControl>
+						<FormLabel htmlFor='password'>
+							<div style={{ position: 'absolute', top: '0'}}>
+                            Password
+							</div>
+						</FormLabel>
+						<Input type={show ? 'text' : 'password'} id='password' pr='4.5rem' top='7' placeholder='Enter password' value={password} onChange={e => setPassword(e.target.value)}/>
+						<Button h='1.95rem'top='-2' left='265' size='sm' onClick={handleClick}>
+							{show ? 'Hide' : 'Show'}
+						</Button>
+					</FormControl>
+
+					<div style={{ position: 'absolute'}}>
+						<Button variantColor="teal" top='6' onClick={handleLogin}>
+                        Login
+						</Button>
+					</div>
 				</form>
 			)}
 			{error && <div style={{color: 'red'}}>{error}</div>}
