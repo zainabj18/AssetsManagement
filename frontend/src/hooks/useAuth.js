5b560aa60c1e6f031676c7052511df608f48e79b
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-01 23:20:16 +0000

    Require the auth context to load before redirect

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -6,7 +6,8 @@ export const AuthContext = createContext({
 	loggedIn:false,
 	user: null,
 	authError:null,
-	login: () => null
+	login: () => null,
+	loaded: false
 });
 
 export const AuthProvider = ({children}) => {
@@ -14,7 +15,7 @@ export const AuthProvider = ({children}) => {
 	const [user, setUser] = useState(null);
 	const [loggedIn,setLoggedIn]=useState(false);
 	const [authError, setAuthError] = useState(null);
-
+	const [loaded,setLoaded]=useState(false);
 	const login = ( username, password ) => {
 		loginUser({ username, password }).then(res=> {
 			setUser(res.data);
@@ -27,15 +28,22 @@ export const AuthProvider = ({children}) => {
 	};
 
 	useEffect(() => {
-		indentifyUser().then(res => setUser(res.data)).catch(() => {});
+		console.log('is loaded');
+		console.log(loaded);
+		indentifyUser().then(res => {
+			console.log('Auth mounted');
+			console.log(res.data);
+			setUser(res.data);
+			setLoggedIn(true);
+		}).catch(() => {}).finally(setLoaded(true));
 	},[]);
 	const value = useMemo(
-		() => ({loggedIn,user,authError,login}), 
+		() => ({loggedIn,user,authError,login,loaded}), 
 		[user,authError]
 	);
 
 	return ( <AuthContext.Provider value={value}>
-		{children}
+		{loaded && children}
 	</AuthContext.Provider> );
 };
 const useAuth = () => {
