c3b289302eec06745da6a770e2d79124a7e4a0f4
Higashiuezato Takase (2021) ZKAC332 <takase.higashiuezato.2021@live.rhul.ac.uk>
2023-02-01 13:31:57 +0000

    Added sent login backend error massage

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -8,8 +8,13 @@ import {
 	InputRightElement,
 	Container,
 	VStack,
-	IconButton
+	IconButton,
+	Alert,
+	AlertIcon,
+	AlertTitle,
+	AlertDescription,
 } from '@chakra-ui/react';
+import axios from 'axios';
 import { ViewIcon,ViewOffIcon } from '@chakra-ui/icons';
 
 function Login() {
@@ -17,33 +22,15 @@ function Login() {
 	const [password, setPassword] = useState('');
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	const [show, setShow] = React.useState(false);
-	const [error, setError] = useState('');
+	const [error, setError] = useState(null);
 	const handleLogin = () => {
-		try {
-			console.log('I am here');
-			fetch('/api/v1/auth/login', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'Access-Control-Allow-Origin': 'true',
-				},
-				body: JSON.stringify({ username, password }),
-			}).then((res) =>
-				res.json().then((data) => {
-					console.log('hello');
-					console.log('Bye');
-					console.log(data);
-					if (data.isAuthenticated) {
-						setIsAuthenticated(true);
-						setError('');
-					}
-				})
-			);
-		} catch (error) {
-			console.error(error);
-			setError('Invalid username or password');
+		axios.post('/api/v1/auth/login',{username, password}).then((res) => {
+			console.log(res);
+			setError(null);
 		}
-	};
+		).catch( (res) =>{
+			setError(res.response.data);
+		});};
 
 	return (
 		<Container>
@@ -73,6 +60,11 @@ function Login() {
 						</InputRightElement>
 					</InputGroup>
 				</FormControl>
+				{error && (<Alert status='error'>
+  							<AlertIcon />
+					<AlertTitle>{error.error}</AlertTitle>
+					<AlertDescription>{error.msg}</AlertDescription>
+				</Alert>)}
 				<Button onClick={handleLogin}>
           Login
 				</Button>
