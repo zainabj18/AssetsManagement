95fe3ed49c25911804421a6807224b3066bbee46
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-02 18:42:30 +0000

    Added user profile icon

--- frontend/src/components/UserMenuButton.js
+++ frontend/src/components/UserMenuButton.js
@@ -1,10 +1,11 @@
 import { Menu,MenuButton,MenuList,Button,MenuItem} from '@chakra-ui/react';
-
+import { FaUserCircle } from 'react-icons/fa';
 const UserMenuButton = () => {
 	return ( <Menu>
-		<MenuButton as={Button} colorScheme='pink'>
-          Profile
+		<MenuButton as={Button} colorScheme='pink' leftIcon={<FaUserCircle size={18} />}>
+            Profile
 		</MenuButton>
+
 		<MenuList>
 			<MenuItem>Logout</MenuItem>
 		</MenuList>
