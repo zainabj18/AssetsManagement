fd3a1342ab82e920342a54ee73791c2cd4676702
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-02 19:36:09 +0000

    Styled the user menu button

--- frontend/src/components/UserMenuButton.js
+++ frontend/src/components/UserMenuButton.js
@@ -1,4 +1,4 @@
-import { Menu,MenuButton,MenuList,Button,MenuItem} from '@chakra-ui/react';
+import { Menu,MenuButton,MenuList,Button,MenuItem,MenuGroup,MenuDivider, Badge, HStack} from '@chakra-ui/react';
 import { FaUserCircle } from 'react-icons/fa';
 import useAuth from '../hooks/useAuth';
 const UserMenuButton = () => {
@@ -9,8 +9,19 @@ const UserMenuButton = () => {
 		</MenuButton>
 
 		<MenuList>
+			<MenuGroup title='Profile'>
+				<HStack>
+					<Badge>{user.userRole}</Badge>
+					<Badge>{user.userPrivileges}</Badge>
+				</HStack>
+				<MenuItem>View profile</MenuItem>
+				<MenuItem>View related assets</MenuItem>
+				<MenuItem>View related projects</MenuItem>
+			</MenuGroup>
+			<MenuDivider />
 			<MenuItem>Logout</MenuItem>
 		</MenuList>
+
 	</Menu>);
 };
  
