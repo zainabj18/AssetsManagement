a1e2b05cc1cd8adbf0a2231cc336ba75a0db15fe
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-02-03 18:55:09 +0000

    Added a table to view the types

--- frontend/src/routes/TypeViewer.js
+++ frontend/src/routes/TypeViewer.js
@@ -1,6 +1,103 @@
+import {
+	VStack,
+	Table, Thead, Tbody, Tr, Th, Td, TableContainer, TableCaption,
+	Text
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
 const TypeViewer = () => {
+
+	const [types, setTypes] = useState([
+		/** Dummy Data */
+		{
+			type: 'framework',
+			metadata: [
+				{
+					attributeName: 'programming Language(s)',
+					attributeType: 'text',
+				},
+				{
+					attributeName: 'public',
+					attributeType: 'checkbox',
+				},
+				{
+					attributeName: 'no. of issues',
+					attributeType: 'number',
+				},
+				{
+					attributeName: 'built on',
+					attributeType: 'datetime-local',
+				},
+				{
+					attributeName: 'version',
+					attributeType: 'text',
+				},
+				{
+					attributeName: 'stars',
+					attributeType: 'num_lmt',
+					validation: {
+						min: 1,
+						max: 5
+					}
+				},
+				{
+					attributeName: 'license',
+					attributeType: 'options',
+					validation: {
+						values: ['MIT', 'GNU'],
+						isMulti: true
+					}
+				},
+				{
+					attributeName: 'authors',
+					attributeType: 'list',
+					validation: {
+						type: 'text'
+					}
+				},
+				{
+					attributeName: 'authors_emails',
+					attributeType: 'list',
+					validation: {
+						type: 'email'
+					}
+				},
+				{
+					attributeName: 'authors_emails_domain',
+					attributeType: 'list',
+					validation: {
+						type: 'url'
+					}
+				}]
+		}
+		/** End of Dummy Data */
+	]);
+
 	return (
-		<h1>Type Viewer</h1>
+		<VStack>
+			<Text>Type Viewer</Text>
+			<TableContainer>
+				<Table varient='simple'>
+					<TableCaption placement='top' color='white'>Types</TableCaption>
+					<Thead>
+						<Tr>
+							<Th color='white'>Type</Th>
+							<Th color='white'>Attributes</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{types.map((types) => {
+							return (
+								<Tr key={types.type}>
+									<Td>{types.type}</Td>
+									<Td>{'!Not implimented!'}</Td>
+								</Tr>
+							);
+						})}
+					</Tbody>
+				</Table>
+			</TableContainer>
+		</VStack>
 	);
 };
 
