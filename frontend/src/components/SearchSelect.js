6a152c5f90966f671e43c362c593e18ed0a7b840
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-05 01:39:01 +0000

    Moved input to popover anchor

--- frontend/src/components/SearchSelect.js
+++ frontend/src/components/SearchSelect.js
@@ -1,37 +1,52 @@
-import {Box, Button, Input,Text,Menu,MenuButton,MenuItemOption,MenuList,Popover,PopoverContent,PopoverTrigger, Portal, UnorderedList, PopoverFooter, ButtonGroup, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody,useDisclosure} from '@chakra-ui/react';
+import {Box, IconButton, Input,Text,Menu,MenuButton,MenuItemOption,MenuList,Popover,PopoverContent,PopoverTrigger, Portal, UnorderedList, PopoverFooter, ButtonGroup, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody,useDisclosure, useBoolean, HStack, Icon, PopoverAnchor} from '@chakra-ui/react';
 import { useEffect,useRef,useState } from 'react';
+import {EditIcon,CheckIcon} from '@chakra-ui/icons';
 import tags from '../mock_tags.json';
 const SearchSelect = () => {
 	const [data, setData] = useState([]);
 	const [results, setResults] = useState([]);
-	const { isOpen, onToggle, onClose } = useDisclosure();
+	const [isEditing, setIsEditing] = useBoolean();
 
 	
 
 	const getData=()=>{
 		setData(tags.data);
+		setResults(tags.data.slice(0,5));
 	};
 
 	useEffect(() => {
 		getData();
-		setResults(data.slice(0,5));
 	}, []);
     
 	return ( 
 		<>
-			
 			<Popover
 				placement='bottom'
-				isOpen={isOpen}
-				onClose={onClose}
+				isOpen={isEditing}
+				onOpen={setIsEditing.on}
+				onClose={setIsEditing.off}
+				closeOnBlur={false}
+				isLazy
+				lazyBehavior='keepMounted'
 			>
-				<PopoverTrigger>
-					<Input mr={5} onClick={onToggle}/>
-				</PopoverTrigger>
+				<HStack>
+					<PopoverAnchor>
+						<Input type={'text'}/>
+					</PopoverAnchor>
+					<PopoverTrigger>
+						<IconButton size='sm' icon={isEditing ? <CheckIcon /> : <EditIcon />} />
+					</PopoverTrigger>
+				</HStack>
+				
 				<PopoverContent>
-					{results.length>0 &&
-                    results.map((d)=>{return (<Box>{d.name}</Box>);})
-					}
+					<PopoverBody>
+						{results.length>0 &&
+                    results.map((d)=>{return (<Box
+                    	_hover={{ fontWeight: 'semibold',background: 'white',
+                    		color: 'teal.500', }}
+                    >{d.name}</Box>);})
+						}
+					</PopoverBody>
 				</PopoverContent>
 			</Popover>
 
