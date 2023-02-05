407efdad7c9d71770bd154e1b883ab96a3e49b0d
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-05 00:49:50 +0000

    Changed to popover component

--- frontend/src/components/SearchSelect.js
+++ frontend/src/components/SearchSelect.js
@@ -1,8 +1,12 @@
-import { Input,Popover,PopoverContent,PopoverTrigger } from '@chakra-ui/react';
-import { useEffect,useState } from 'react';
+import {Box, Button, Input,Text,Menu,MenuButton,MenuItemOption,MenuList,Popover,PopoverContent,PopoverTrigger, Portal, UnorderedList, PopoverFooter, ButtonGroup, PopoverHeader, PopoverArrow, PopoverCloseButton, PopoverBody,useDisclosure} from '@chakra-ui/react';
+import { useEffect,useRef,useState } from 'react';
 import tags from '../mock_tags.json';
 const SearchSelect = () => {
 	const [data, setData] = useState([]);
+	const [results, setResults] = useState([]);
+	const { isOpen, onToggle, onClose } = useDisclosure();
+
+	
 
 	const getData=()=>{
 		setData(tags.data);
@@ -10,24 +14,24 @@ const SearchSelect = () => {
 
 	useEffect(() => {
 		getData();
+		setResults(data.slice(0,5));
 	}, []);
     
 	return ( 
 		<>
-			<Input type="text"/>
-			{data.length>0 && <Popover>
+			
+			<Popover
+				placement='bottom'
+				isOpen={isOpen}
+				onClose={onClose}
+			>
 				<PopoverTrigger>
-					<Input type="text"/>
+					<Input mr={5} onClick={onToggle}/>
 				</PopoverTrigger>
 				<PopoverContent>
-					{data.map((tag)=>{
-						return (<div>{tag.name}</div>);
-					})}
+					<Text>Hello</Text>
 				</PopoverContent>
-
 			</Popover>
-
-			}
 
 		</> );
 };
