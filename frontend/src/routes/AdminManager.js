ca6b674661bc32a77fe2fe99f1b155c7b9a24281
Zayn Rashid <rangedbear@gmail.com>
2023-02-08 21:42:54 +0000

    Implemented basic table to display user details and filled it with dummy info.

--- frontend/src/routes/AdminManager.js
+++ frontend/src/routes/AdminManager.js
@@ -1,15 +1,61 @@
 import { Outlet } from 'react-router-dom';
-import { Heading, VStack,Text } from '@chakra-ui/react';
+import { Heading, VStack,Text, HStack } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import { Input, Stack } from '@chakra-ui/react';
+import {
+	Table,
+	Thead,
+	Tbody,
+	Tfoot,
+	Tr,
+	Th,
+	Td,
+	TableCaption,
+	TableContainer,
+} from '@chakra-ui/react';
 
 const AdminManager = () => {
 	return (
 		<VStack minW="100vw">
 			<Text>AdminManager</Text>
 			<Stack spacing={3} color={'black'}>
-				<Input bg='white' placeholder='search' size='lg' type='text' width={800} top={100}/>
+				<Input bg='white' placeholder='search' size='lg' type='text' width={800} top={70}/>
 			</Stack>
+			<Stack pt={125}>
+				<TableContainer color='white'>
+					<Table variant='simple' size={'lg'}>
+						<Thead>
+							<Tr>
+								<Th color={'white'}>First Name</Th>
+								<Th color={'white'}>Last Name</Th>
+								<Th color={'white'}>Username</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							<Tr>
+								<Td>John</Td>
+								<Td>Plat</Td>
+								<Td>@John</Td>
+							</Tr>
+							<Tr>
+								<Td>Ben</Td>
+								<Td>Hatch</Td>
+								<Td>@Ben</Td>
+							</Tr>
+							<Tr>
+								<Td>Ben</Td>
+								<Td>Smith</Td>
+								<Td>@Ben.Smith</Td>
+							</Tr>
+							<Tr>
+								<Td>Kate</Td>
+								<Td>Barlow</Td>
+								<Td>@Kate</Td>
+							</Tr>
+						</Tbody>
+					</Table>
+				</TableContainer>
+			</Stack>	
 		</VStack>
 	);
 };
