ded0aac7ff574cf00c727337abfb916e26a65ab1
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-08 11:10:12 +0000

    Added endpoint to get a list of types

--- backend/app/type/routes.py
+++ backend/app/type/routes.py
@@ -1,19 +1,18 @@
-from datetime import datetime, timedelta
+from flask import Blueprint
+from psycopg.rows import dict_row
+from app.db import get_db
 
-import jwt
-from flask import Blueprint, current_app, jsonify, request
-from psycopg import Error
-from psycopg.rows import class_row
-from pydantic.error_wrappers import ValidationError
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from app.core.utils import protected
-from app.db import UserRole, get_db
-from app.schemas import UserCreate, UserInDB
-
+def get_types(db):
+    with db.connection() as db_conn:
+        with db_conn.cursor(row_factory=dict_row) as cur:
+            cur.execute("""SELECT * FROM types;""")
+            return cur.fetchall()
 bp = Blueprint("type", __name__, url_prefix="/type")
-
-
 @bp.route("/adder", methods=["GET"])
 def types():
     return {"msg": ""}, 200
+
+@bp.route("/", methods=["GET"])
+def list():
+    db = get_db()
+    return {"msg": "types","data":get_types(db)}, 200
--- backend/tests/integration/test_type.py
+++ backend/tests/integration/test_type.py
@@ -2,3 +2,24 @@
 def test_typeAdder_route(client):
     res = client.get("/api/v1/type/adder")
     assert res.status_code == 200
+
+
+def test_type_list(client,db_conn):
+    expected_types=[]
+    with db_conn.cursor() as cur:
+        for x in range(100):
+            name=f"Test-{x}"
+            type={'name': name}
+            cur.execute(
+        """
+    INSERT INTO types (name)
+VALUES (%(name)s) RETURNING id;""",
+        type,
+    )
+            id=cur.fetchone()[0]
+            type['id']=id
+            expected_types.append(type)
+        db_conn.commit()
+    res = client.get("/api/v1/type/")
+    assert res.status_code == 200
+    assert res.json=={"msg": "types","data":expected_types}
