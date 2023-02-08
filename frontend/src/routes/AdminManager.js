16b32188a78ba25e78d1a8b64ce9649032b37069
Zayn Rashid <rangedbear@gmail.com>
2023-02-08 21:04:27 +0000

    Implemented search bar component.

--- frontend/src/routes/AdminManager.js
+++ frontend/src/routes/AdminManager.js
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Heading, VStack,Text } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
+import { Input, Stack } from '@chakra-ui/react';
 
 const AdminManager = () => {
 	return (
 		<VStack minW="100vw">
 			<Text>AdminManager</Text>
+			<Stack spacing={3} color={'black'}>
+				<Input bg='white' placeholder='search' size='lg' type='text' width={800} top={100}/>
+			</Stack>
 		</VStack>
 	);
 };
