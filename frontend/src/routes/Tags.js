6e52a58ca54c5697cecee118637e66cc7f02950a
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-16 22:07:16 +0000

    Added fetch to get tags from db

--- frontend/src/routes/Tags.js
+++ frontend/src/routes/Tags.js
@@ -1,12 +1,36 @@
-import { Box, Button, Flex, HStack, Input, } from '@chakra-ui/react';
+import { Box, Flex, HStack, Input, VStack, } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { fetchTags } from '../api';
+import CustomNavLink from '../components/CustomNavLink';
 import NewTag from '../components/NewTag';
 
 const Tags = () => {
+	const [tags, setTags] = useState([]);
+
+	useEffect(() => {
+		fetchTags().then((res)=>{
+			setTags(res.data);
+		}
+		);
+	
+	}, []);
+	
 	return (<Flex w='100%' minH='80vh' alignItems={'stretch'} p={2} border>
-		<HStack w='30%' minH='100%' bg='gray.300' p={4} color='black' align={'top'}>
-			<Input type='text' placeholder='Search for tag ...'/>
-			<NewTag />
-		</HStack>
+		<Box w='30%' minH='100%' bg='gray.300' p={4} color='black' align={'top'}>
+			<HStack>
+				<Input type='text' placeholder='Search for tag ...'/>
+				<NewTag />
+			</HStack>
+			<VStack p={2}>
+				{tags.map((t,index)=>(
+					<CustomNavLink key={index} to={`./${t.id}`} w='100%'>
+						{t.name}
+					</CustomNavLink>
+
+				))}
+				
+			</VStack>
+		</Box>
 		<Box w='70%' minH='100%' bg='white' />
 	</Flex>
 	);
