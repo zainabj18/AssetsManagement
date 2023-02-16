f1bf7bba8aa6648e84a657d70e3dc07907d4b1c1
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-16 18:09:51 +0000

    Created layout grid for tag sidebar

--- frontend/src/routes/Tags.js
+++ frontend/src/routes/Tags.js
@@ -1,5 +1,12 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 const Tags = () => {
-	return ( <h1>Tags</h1> );
+	return (<Grid w='100%' templateColumns='repeat(5, 1fr)'>
+		<GridItem colSpan={1}
+			w='100%' h='10' bg='red.500' />
+		<GridItem colSpan={4}
+			w='100%' h='10' bg='gray.500' />
+	</Grid>);
 };
  
 export default Tags;
