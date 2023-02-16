5970505cf6f00be31fc3c25420bf7c2b1f28deae
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-16 21:49:22 +0000

    Added new tag draw to create tag

--- frontend/src/routes/Tags.js
+++ frontend/src/routes/Tags.js
@@ -1,10 +1,11 @@
 import { Box, Button, Flex, HStack, Input, } from '@chakra-ui/react';
+import NewTag from '../components/NewTag';
 
 const Tags = () => {
 	return (<Flex w='100%' minH='80vh' alignItems={'stretch'} p={2} border>
 		<HStack w='30%' minH='100%' bg='gray.300' p={4} color='black' align={'top'}>
 			<Input type='text' placeholder='Search for tag ...'/>
-			<Button>Create</Button>
+			<NewTag />
 		</HStack>
 		<Box w='70%' minH='100%' bg='white' />
 	</Flex>
