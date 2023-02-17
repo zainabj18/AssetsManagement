41d7adcd635253a218164ea46eb3b5f128ca3cbf
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-17 17:41:31 +0000

    Create asset table

---
+++ frontend/src/components/AssetTable.js
@@ -0,0 +1,50 @@
+import { Badge,Box } from '@chakra-ui/react';
+import { useEffect, useMemo, useState } from 'react';
+import { fetchAssetClassifications } from '../api';
+import CustomTable from './CustomTable';
+
+const AssetTable = ({assets,setSelectedRows}) => {
+	const [colours,setColors]=useState({});
+	const columns =useMemo(()=>
+	{ return {
+		
+		
+		'asset_id':{
+			header: 'Asset ID',
+			canFilter:true	
+		},
+		'name':{
+			header: 'Asset Name',
+			canFilter:true
+		},
+		'type':{
+			header: 'Asset Type',
+			canFilter:true
+		},
+		'classification':{
+			header: 'Asset Classification',
+			canFilter:true,
+			Cell:(rowID,value)=>{return <Badge bg={colours[value]} color={'white'}>{value}</Badge>;}
+		},
+	};
+	}
+	,[]);
+	useEffect(() => {
+		console.log(assets,'I am assets');
+		fetchAssetClassifications().then((data)=>{
+			let classification_levels=data.data;
+			let customColours={};
+			let RED=225;
+			let GREEN=128;
+			for (let i = 0; i < classification_levels.length; i++) {
+				let factor=i/(classification_levels.length-1);
+				customColours[classification_levels[i]]=`rgb(${factor*RED},${(1-factor)*GREEN},0)`;
+			}
+			setColors(customColours);
+		}).catch((err) => {console.log(err);});},[]);
+	return (
+
+		<CustomTable rows={assets} cols={columns}  setSelectedRows={()=>{}}/>  );
+};
+ 
+export default AssetTable;
--- frontend/src/routes/TagViewer.js
+++ frontend/src/routes/TagViewer.js
@@ -2,49 +2,23 @@ import { Badge, Heading,VStack } from '@chakra-ui/react';
 import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchAssetsinTag } from '../api';
+import AssetTable from '../components/AssetTable';
 import CustomTable from '../components/CustomTable';
 
 const TagViewer = () => {
-	const [assets, setAssets] = useState([]);
+	const [assetsin, setAssets] = useState([]);
 	const [tag, setTag] = useState('');
 	const { id } = useParams();
-
-	const columns =useMemo(()=>
-	{ return {
-		
-		
-		'asset_id':{
-			header: 'Asset ID',
-			canFilter:true	
-		},
-		'name':{
-			header: 'Asset Name',
-			canFilter:true
-		},
-		'type':{
-			header: 'Asset Type',
-			canFilter:true
-		},
-		'classification':{
-			header: 'Asset Classification',
-			canFilter:true,
-			Cell:(rowID,value)=>{return <Badge>{value}</Badge>;}
-		},
-	};
-	}
-	,[]);
-
-	
 
 	useEffect(() => {
 		fetchAssetsinTag(id).then((res)=>{setAssets(res.data.assets);
 			setTag(res.data.tag);});
-		console.log(assets);
+		console.log(assetsin);
 	}, [id]);
     
 	return ( <VStack bg={'whiteAlpha.500'} h={'100%'} w={'100%'} p={2}>
 		<Heading>{tag}</Heading>
-		<CustomTable rows={assets} cols={columns} setSelectedRows={()=>{}}/>
+		<AssetTable assets={assetsin} setSelectedRows={()=>{}}/>
 	</VStack> );
 };
  
