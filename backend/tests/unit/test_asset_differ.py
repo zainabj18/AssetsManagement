155a3d31c4a2ad9a4b801d1b6cc6b92000987dcd
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-20 19:47:56 +0000

    Test that asset_differ detects a removal of keys

--- backend/tests/unit/test_asset_differ.py
+++ backend/tests/unit/test_asset_differ.py
@@ -5,7 +5,7 @@ from app.asset.routes import asset_differ
 
 @pytest.mark.parametrize(
     "new_assets",
-    [{"batch_size": 10,"add_to_db":False}],
+    [{"batch_size": 1,"add_to_db":False}],
     indirect=True,
 )
 def test_removed_empty_on_match(new_assets):
@@ -13,3 +13,16 @@ def test_removed_empty_on_match(new_assets):
     new_asset=old_asset.copy()
     res=asset_differ(old_asset,new_asset)
     assert res["removed"]==[]
+
+@pytest.mark.parametrize(
+    "new_assets",
+    [{"batch_size": 1,"add_to_db":False}],
+    indirect=True,
+)
+def test_remove_from_orginal(new_assets):
+    old_asset=json.loads(new_assets[0].json())
+    new_asset=old_asset.copy()
+    del new_asset["link"]
+    res=asset_differ(old_asset,new_asset)
+    assert set(res["removed"])==set(["link"])
+
