e02cb4a069b12b152050b986ede829ae9362d046
Gurbir74 <zkac396@live.rhul.ac.uk>
2023-02-21 22:47:00 +0000

    Added Delete function in CreateProject.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -38,6 +38,10 @@ export const fetchProjects= async ()=> {
 	return await axios.get('/project/').then(res=>res.data);
 };
 
+export const fetchAllProjects = async () => {
+	return await axios.get('/project/allProjects').then(res => res.data);
+};
+
 export const fetchAllTypes = async () => {
 	return await axios.get('/type/allTypes').then(res => res.data);
 };
@@ -74,6 +78,9 @@ export const deleteAsset = async (id) => {
 	return await axios.delete(`/asset/${id}`).then(res => res.data);
 };
 
+export const deleteProject = async (id) => {
+	return await axios.delete(`/project/${id}`).then(res => res.data);
+};
 export const fetchAssetSummary = async () => {
 	return await axios.get('/asset/summary').then(res => res.data);
 };
--- frontend/src/routes/CreateProject.js
+++ frontend/src/routes/CreateProject.js
@@ -7,16 +7,28 @@ import {
 	VStack,
 	Button,
 	Flex,
+	useBoolean
 } from '@chakra-ui/react';
 //import axios from 'axios';
 import React, { useState } from 'react';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { IconButton } from '@chakra-ui/react';
-import {createProject} from '../api.js';
+import {createProject, deleteProject, fetchAllProjects } from '../api';
+import { useEffect} from 'react';
+
 const CreateProject = () => {
 	const [projects, setProjects] = useState([{ name: '', description: '' }]);
-	//const [display_people_in_project, set_display_people_in_project] = useState(false);
+	const [toggle, set_toggle] = useBoolean();
 
+	useEffect(() => {
+		async function load_allProjects() {
+			let data = await fetchAllProjects(res => res.data);
+			setProjects(data);
+		}
+		load_allProjects();
+	}, [toggle]);
+
+	
 	const handleCreate = (index, event) => {
 		let data = [...projects];
 		data[index][event.target.name] = event.target.value;
@@ -32,11 +44,17 @@ const CreateProject = () => {
 		
 	};
 
-	
-	const deleteProject = (index) => {
-		let data = [...projects];
-		data.splice(index, 1);
-		setProjects(data);
+	const handleDelete = (index) => {
+		const projectToDelete = projects[index];
+		deleteProject(projectToDelete.id).then((data) => {
+			if (data.wasAllowed == false) {
+				alert('Project ' + projects.name + ' is depended upon, can not be deleted.');
+			}
+			else {
+				//setProjects((prevProjects) => prevProjects.filter((p, i) => i !== index));
+				set_toggle.toggle();
+			}
+		});
 	};
 
 	return (
@@ -72,7 +90,7 @@ const CreateProject = () => {
 								left={20}
 								icon={<DeleteIcon />}
 								colorScheme="blue"
-								onClick={() => deleteProject(index)}
+								onClick={() => handleDelete(index)}
 							/>
 						</Box>
 					</VStack>
