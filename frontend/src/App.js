f958ef34efbe90b90ae4e748802d4edc4a381386
Gurbir74 <zkac396@live.rhul.ac.uk>
2023-02-22 13:18:03 +0000

    Updated App.js and edited paths.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import AssetViewer from './components/AssetVeiwer';
 import CreateAsset from './routes/assets/CreateAsset';
 import Layout from './routes/Layout';
 import Login from './routes/Login';
 import NoMatch from './routes/NoMatch';
-import AssetSearcher from './routes/AssetSearcher';
 import User from './routes/User';
 import TypeAdder from './routes/TypeAdder';
 import TypeViewer from './routes/TypeViewer';
 import { AuthProvider } from './hooks/useAuth';
 import AssetsOverview from './routes/assets/AssetsOverview';
 import SubLayout from './routes/assets/SubLayout';
-import AssetList from './components/AssetList';
 import CreateProject from './routes/CreateProject';
-import { Button } from '@chakra-ui/react';
 import AdminManager from './routes/AdminManager';
 import RelatedAssetViewer from './components/RelatedAssetViewer';
 import Tags from './routes/Tags';
 import TagViewer from './routes/TagViewer';
+import ProjectViewer from './routes/ProjectViewer';
 //TODO:Wrap in error boundary
 function App() {
 
@@ -35,9 +33,7 @@ function App() {
 					<Route path="tags/" element={<Tags />}>
 						<Route path=":id" element={<TagViewer/>} />	
 					</Route>
-					<Route path="projects/" element={<SubLayout name="Projects"/>}>
-						<Route index element={<NavLink to="./new">Create New Project</NavLink>} />
-						<Route path="new" element={<CreateProject />} />
+					<Route path="projects/" element={<ProjectViewer/>}>
 					</Route>
 					<Route path="type/" element={<SubLayout name="Types"/>}>
 						<Route index element={<TypeViewer />} />
@@ -45,6 +41,7 @@ function App() {
 					</Route>
 					<Route path="accounts" element={<AdminManager />} />
 					<Route path="user" element={<User />} />
+					<Route path="new" element={<CreateProject />} />
 				</Route>
 				<Route path="*" element={<NoMatch />} />
 			</Routes>
