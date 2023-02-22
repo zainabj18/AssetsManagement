e461de649995805be9cb0b9b3135b23e59592ae5
Dmani Barnett <dmani.barnett@gmail.com>
2023-02-22 11:25:35 +0000

    Added route for fetching relatated tags of an asset

--- backend/app/asset/routes.py
+++ backend/app/asset/routes.py
@@ -426,3 +426,42 @@ INNER JOIN assets_in_tags ON assets.asset_id=assets_in_tags.asset_id WHERE soft_
                     assets_json.append(aj)
             res = jsonify({"data": {"tag": tag, "assets": assets_json}})
     return res
+
+
+@bp.route("/related/tags/<id>", methods=["GET"])
+@protected(role=UserRole.VIEWER)
+def related_tags(id,user_id, access_level):
+    db = get_db()
+    # get related assets for  an asset and set them to be selected for easy rendering on UI
+    assets_json = []
+    with db.connection() as db_conn:
+        with db_conn.cursor(row_factory=class_row(AssetBaseInDB)) as cur:
+            cur.execute(
+                """
+                WITH related_asset_tags as (SELECT COUNT(asset_id),asset_id FROM assets_in_tags WHERE tag_id in (SELECT tag_id FROM assets_in_tags WHERE asset_id=%(id)s) and asset_id !=%(id)s
+GROUP BY asset_id
+HAVING COUNT(asset_id)>0)
+SELECT assets.*,related_asset_tags.count FROM assets
+INNER JOIN related_asset_tags on assets.asset_id=related_asset_tags.asset_id
+ORDER BY count DESC;""",
+                {"id": id},
+            )
+            selected_assets = list(cur.fetchall())
+ 
+            assets = list(cur.fetchall())
+            selected_assets.extend(assets)
+        # gets the type name for each assset
+        with db_conn.cursor(row_factory=dict_row) as cur:
+            for a in selected_assets:
+                if a.classification <= access_level:
+                    cur.execute(
+                        """SELECT type_name FROM types WHERE type_id=%(id)s;""",
+                        {"id": a.type},
+                    )
+                    type = cur.fetchone()["type_name"]
+                    aj = json.loads(a.json(by_alias=True))
+                    aj["type"] = type
+                    assets_json.append(aj)
+            res = jsonify({"data": assets_json})
+    return res
+
