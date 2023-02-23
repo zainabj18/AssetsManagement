d2e25d2be093b7fa66527a5c31a55cadbfe6eac2
Zainab Jaffar <zkac205@live.rhul.ac.uk>
2023-02-23 13:07:56 +0000

    created a table to display the projects created

---
+++ frontend/src/components/PeopleLink.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { useTable, useRowSelect, useGlobalFilter } from 'react-table';
+
+import {
+	Table,
+	Thead,
+	Tbody,
+	Tr,
+	Th,
+	Td,
+	TableContainer,
+	Link
+} from '@chakra-ui/react';
+import { Link as NavLink } from 'react-router-dom';
+import GlobalFilter from './GlobalFilter';
+
+
+function ProjectList({projects}) {
+	const data = React.useMemo(
+		() => projects,
+		[projects]
+	);
+
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: 'Project ID',
+				accessor: 'project_id',
+				Cell:row=><Link to={`view/${row.value}`} as={NavLink}>{row.value}</Link>
+			},
+			{
+				Header: 'Project Name',
+				accessor: 'name',
+			},
+			{
+				Header: 'Project Type',
+				accessor: 'type',
+			},
+			{
+				Header: 'Project members',
+				accessor: 'members',
+			}
+		],
+		[]
+	);
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		state,
+		prepareRow,
+		selectedFlatRows,
+		setGlobalFilter,
+	} = useTable(
+		{
+			columns,
+			data,
+		},
+		useRowSelect,
+		useGlobalFilter
+	);
+
+
+	return (<>
+
+		<TableContainer>
+			<GlobalFilter
+				globalFilter={state.globalFilter}
+				setGlobalFilter={setGlobalFilter}
+			/>
+			<Table {...getTableProps()} color={'white'}>
+				<Thead >
+					{headerGroups.map(headerGroup => (
+						<Tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map(column => (
+								<Th
+									{...column.getHeaderProps()}
+									color={'white'}
+								>
+									{column.render('Header')}
+								</Th>
+							))}
+						</Tr>
+					))}
+				</Thead>
+				<Tbody {...getTableBodyProps()}>
+					{rows.map(row => {
+						prepareRow(row);
+						return (
+							<Tr {...row.getRowProps()}>
+								{row.cells.map(cell => {
+									return (
+										<Td
+											{...cell.getCellProps()}
+										>
+											{cell.render('Cell')}
+										</Td>
+									);
+								})}
+							</Tr>
+						);
+					})}
+				</Tbody>
+			</Table>
+			<pre>
+				<code>
+					{console.log(selectedFlatRows.map(
+						d => d.original.id
+
+					))}
+				</code>
+			</pre>
+		</TableContainer>
+
+	</>
+	);
+}
+
+export default ProjectList;
