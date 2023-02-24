6c1df237da369148349508cf5081ec2b6a48795d
PARKASH Deepak (2021) WJIS333 <deepak.parkash.2021@live.rhul.ac.uk>
2023-02-24 05:57:34 +0000

    added Database schema

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -54,3 +54,21 @@ def close_db(e=None):
     db = g.pop('db', None)
     if db is not None:
         db.close()
+
+        class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    asset_id = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'asset_id': self.asset_id,
+            'username': self.username,
+            'content': self.content,
+            'timestamp': self.timestamp.isoformat()
+        }
