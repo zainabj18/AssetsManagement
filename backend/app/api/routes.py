91e4763c3f0cc27e6b48149eba7831b36ef1e178
PARKASH Deepak (2021) WJIS333 <deepak.parkash.2021@live.rhul.ac.uk>
2023-02-24 05:56:17 +0000

    added Backend API endpoint to save the comment

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -7,3 +7,17 @@ bp = Blueprint("api", __name__,url_prefix=settings.APPLICATION_ROOT_URL)
 def index():
     return {"msg":"Hello World!","version":settings.API_VERSION,"url":settings.APPLICATION_ROOT_URL}
 bp.register_blueprint(auth_bp)
+
+api_bp = Blueprint('api', __name__, url_prefix='/api')
+
+@api_bp.route('/assets/<int:asset_id>/comments', methods=['POST'])
+def create_comment(asset_id):
+    data = request.json
+    if not data or not isinstance(data.get('username'), str) or not isinstance(data.get('content'), str):
+        raise BadRequest('Invalid request data')
+
+    comment = Comment(asset_id=asset_id, username=data['username'], content=data['content'])
+    db.session.add(comment)
+    db.session.commit()
+
+    return jsonify(comment.to_dict()), 201
