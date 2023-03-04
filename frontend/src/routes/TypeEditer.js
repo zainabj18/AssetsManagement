2f55098b6a3f16b4e5ce756243ee472a23b3538e
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-04 13:32:52 +0000

    The type is now loaded to the page upon loading the page.

--- frontend/src/routes/TypeEditer.js
+++ frontend/src/routes/TypeEditer.js
@@ -1,6 +1,28 @@
+import { Button, Text, useBoolean } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchType } from '../api';
+
 const TypeEditer = () => {
+	let { id } = useParams(); 
+
+	const [toggle, set_toggle] = useBoolean();
+
+	const [type, set_type] = useState([]);
+
+	useEffect(() => {
+		async function load_type() {
+			let data = await fetchType(id, res => res.data);
+			set_type(data);
+		}
+		load_type();
+	}, [toggle]);
+
 	return (
-		<h1>TypeEditer</h1>
+		<>
+			<Text>TypeEditer</Text>
+			<Button onClick={() => console.log(type)}>Log Type</Button>
+		</>
 	);
 };
 
