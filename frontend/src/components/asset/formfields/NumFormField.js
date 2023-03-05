86c09c9f96021adc953e57d444e001181d499a68
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-05 15:05:42 +0000

    Fixed bug unvalidated min

---
+++ frontend/src/components/asset/formfields/NumFormField.js
@@ -0,0 +1,32 @@
+import {
+	FormControl,
+	FormLabel,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper
+} from '@chakra-ui/react';
+
+const NumFormField = ({fieldName,fieldDefaultValue,validation,onChangeHandler,isDisabled}) => {
+	return (
+		<FormControl >
+			<FormLabel>{fieldName}</FormLabel>
+			<NumberInput 
+				defaultValue={fieldDefaultValue} 
+				min={validation.min} 
+				max={validation.max} 
+				onChange={(val) => onChangeHandler(fieldName,val)}
+				isDisabled={isDisabled}
+			>
+				<NumberInputField />
+				<NumberInputStepper >
+					<NumberIncrementStepper color={'blue.100'}/>
+					<NumberDecrementStepper color={'blue.100'}/>
+				</NumberInputStepper>
+			</NumberInput>
+		</FormControl>
+	);
+};
+
+export default NumFormField;
