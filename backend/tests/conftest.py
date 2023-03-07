51212d6e5acf64f64396d38357ca3b88a78c20d0
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-07 18:55:05 +0000

    Added new fixture for creating multiple type versions

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
 from app.db import UserRole, DataAccess
 from app.schemas import AssetBaseInDB
+from app.schemas.factories import TypeVersionFactory,TypeFactory
 import jwt
 import pytest
 from app import create_app
 from app.db import get_db, init_db,create_assets
 from flask import current_app
-
 
 @pytest.fixture
 def flask_app():
@@ -78,3 +78,34 @@ def expected_res(request):
 def new_assets(db_conn, request):
     return create_assets(db_conn,request.param["batch_size"],request.param.get("add_to_db"))
     
+
+@pytest.fixture(params=[
+        {"size":1,"add_to_db": False}
+    ])
+def type_verions(db_conn,request):
+    batch_size=request.param["size"]
+    added_versions=[]
+    new_type=TypeFactory.build(type_id=1)
+    verion_ids=set()
+    batch_size_counter=batch_size
+    while len(added_versions)<batch_size:
+        batch_result = TypeVersionFactory.batch(size=batch_size_counter,type_id=1) 
+        for type_verion in batch_result:
+            if type_verion.version_id not in verion_ids:
+                verion_ids.add(type_verion.version_id)
+                added_versions.append(type_verion)
+        batch_size_counter=batch_size-len(added_versions)
+    with db_conn.cursor() as cur:
+        cur.execute(
+                        """
+                INSERT INTO types (type_id,type_name)
+            VALUES (%(type_id)s,%(type_name)s) ON CONFLICT (type_name) DO NOTHING;""",
+                        new_type.dict(),
+                    )
+        for type_version in added_versions:
+            cur.execute(
+                                """
+                        INSERT INTO type_version (version_id,version_number,type_id)
+                    VALUES (%(version_id)s,%(version_number)s,%(type_id)s);""",
+                                type_version.dict(),
+                            )
