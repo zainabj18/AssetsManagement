9b0bc2e39f54a72a80cbf0cdc67bf0352aa73568
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-08 18:55:18 +0000

    Updated upgrade availibale test for what data should be returned

--- backend/tests/integration/test_assets.py
+++ backend/tests/integration/test_assets.py
@@ -422,16 +422,46 @@ def test_upgrade_availiable(db_conn,client,new_assets,type_verions):
 SET type_id = %(type_id)s""",
             {"type_id": type_id},
         )
-        print(sorted([row.version_number for row in type_verions[0]]))
         min_version_number=min([row.version_number for row in type_verions[0]])
         cur.execute(
             """UPDATE type_version
 SET version_number = %(version_number)s WHERE version_id=%(version_id)s""",
             {"version_number":min_version_number-1,"version_id":new_assets[0].version_id},
         )
+        cur.execute(
+            """SELECT MAX(version_id) FROM type_version;""")
         db_conn.commit()
+        max_version_id=cur.fetchone()[0]
+        max_version_attributes_id=[]
+        max_version_attributes=[]
+        for row in type_verions[0]:
+            if row.version_id==max_version_id:
+                for attribute in row.attributes:
+                    max_version_attributes_id.append(attribute.attribute_id)
+                    max_version_attributes.append(attribute)
+        print(max_version_attributes_id)
+        cur.execute(
+            """SELECT attribute_id FROM attributes_in_types WHERE type_version=%(type_version)s ;""",{"type_version":new_assets[0].version_id})
+        old_version_attributes_id=[row[0] for row in cur.fetchall()]
+       
         res = client.get(f"/api/v1/asset/upgrade/{new_assets[0].asset_id}")
         assert res.status_code == 200
         assert res.json["msg"] == "upgrade needed"
-        assert res.json["data"] == []
         assert res.json["canUpgrade"]==True
+        assert len(res.json["data"])==2
+        new_attributes_counter=0
+        for a in max_version_attributes:
+            if a.attribute_id not in old_version_attributes_id:
+                att=a.dict(by_alias=True,exclude={"attribute_value"}) 
+                assert att in res.json["data"][0]
+                new_attributes_counter+=1
+            else:
+                old_version_attributes_id.remove(a.attribute_id)
+        assert len(res.json["data"][0])==new_attributes_counter
+        print(old_version_attributes_id)
+        cur.execute(
+            """SELECT attribute_name FROM attributes WHERE attribute_id=ANY(%(attribute_ids)s);""",{"attribute_ids":old_version_attributes_id})
+        removed_names=[row[0] for row in cur.fetchall()]
+
+        print(removed_names)
+        assert set(res.json["data"][1])==set(removed_names)
