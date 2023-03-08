a82ce2c742f6e2419dc50b727f6229090d4fe8a4
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-08 16:05:22 +0000

    Updated factories to return empty dict if not attribute validation required

--- backend/app/schemas/factories.py
+++ backend/app/schemas/factories.py
@@ -13,7 +13,7 @@ def add_validation_json(name: str, values: dict, *args, **kwds):
         return {"type": "text"}
     if values["attribute_type"] == "options":
         return {"values": f.words(10), "isMulti": True}
-    return None
+    return {}
 
 
 def add_value(name: str, values: dict, *args, **kwds):
