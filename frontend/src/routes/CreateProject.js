18f13efd0aab75ac58d2adbdee82e4819aeee752
Higashiuezato Takase (2021) ZKAC332 <takase.higashiuezato.2021@live.rhul.ac.uk>
2023-03-08 11:56:58 +0000

    Changed some features on test_project.py and CreateProject.js

--- frontend/src/routes/CreateProject.js
+++ frontend/src/routes/CreateProject.js
@@ -7,17 +7,24 @@ import {
 	VStack,
 	Button,
 	Flex,
-	Checkbox
+	Checkbox, useBoolean,
+	HStack
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
-import {createProject} from '../api.js';
+import {createProject, fetchPeople} from '../api.js';
+import NewTag from '../components/NewTag';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 
 
 const CreateProject = () => {
 	const[newName, setNewName] = useState('');
 	const[newDescription, setNewDescription] = useState('');
+	const [toggle, set_toggle] = useBoolean();
+	const [trigger,setTrigger]=useBoolean();
+
 	let navigate=useNavigate();
 
 	const changeName = (name) => {
@@ -29,7 +36,7 @@ const CreateProject = () => {
 	};
 
 	const addProject = () => {
-		createProject({ name: newName, description: newDescription, accountID: selectedPeople }).then(_ => {
+		createProject({ name: newName, description: newDescription, accounts: selectedPeople }).then(_ => {
 			navigate('../');
 		});
 	};
@@ -47,51 +54,90 @@ const CreateProject = () => {
 		}
 		setSelectedPeople(list);
 	};
+	
+	const[allPeople, setAllPeople] = useState([]);
+	
+	  useEffect(() => {
+		async function load_allPeople() {
+			let data = await fetchPeople();
+			console.log(data);
+			setAllPeople(data.data);
+		}
+		load_allPeople();
+	}, [toggle]);
 
 
+	return (<Flex w='100%' minH='80vh' alignItems={'stretch'} p={2} border>
+		{/* <Text fontSize='3xl'>Create Project</Text> */}
+		<Box w='30%' minH='100%' bg='gray.300' p={4} color='black' align={'top'}>
+			<HStack>
+				<Input
+					type="text"
+					placeholder="Project Name"
 
-	return (
-		<VStack minW="100vw" spacing={3}>
-			<Text fontSize='3xl'>Create Project</Text>
-			<Box bg='white' w='100%' p={7} color='black'>
-				<FormControl isRequired>
-					<FormLabel color={'black'}>Project Name</FormLabel>
-					<Input
-						type="text"
-						placeholder="Project Name"
-
-						onChange={(event) => changeName(event.target.value)}
-						name="name"
-					/>
-				</FormControl>
-				<FormControl isRequired>
-					<FormLabel color={'black'}>Description</FormLabel>
-					<Input
-						type="text"
-						placeholder="Description"
-						onChange={(event) => changeDescription(event.target.value)}
-						name="description"
-					/>
-				</FormControl>
-			</Box>
-			<Flex minWidth='max-content' alignItems='center' gap='4'>
-				<Button onClick={addProject}>Save Project</Button>
-			</Flex>
+					onChange={(event) => changeName(event.target.value)}
+					name="name"
+				/>
+				<NewTag trigger={setTrigger}/>
+			</HStack>
+			<HStack>
+				<Input
+					type="text"
+					placeholder="Description"
+					onChange={(event) => changeDescription(event.target.value)}
+					name="description"
+				/>
+				<NewTag trigger={setTrigger}/>
+			</HStack>
+		</Box>
+		<VStack p={2}>
 			{allPeople.map((person, index) => {
 				return (
 					<VStack key={person.accountID} align="left">
-						<Checkbox>
-							onChange={(e) => adjustSelectedPeople(e.target.checked, index)}
+						<Checkbox
+							onChange={(e) => adjustSelectedPeople(e.target.checked, person.accountID)}>
 						    {person.username}
 						</Checkbox>
 					</VStack>
 				);
 			})}
-			
 		</VStack>
+		<HStack>
+			<Button onClick={addProject}>Save Project</Button>
+		</HStack>
+		<Box w='70%' minH='100%' bg='white' alignItems='center'>
+			<Outlet/>
+		</Box>		
+	</Flex>
 
 	);
+
+
+
+
+	
+	// 	return (
+	// 		<Flex w='100%' minH='80vh' alignItems={'stretch'} p={2} border>
+	// 			<Box w='30%' minH='100%' bg='gray.300' p={4} color='black' align={'top'}>
+	// 				<HStack>
+	// 					<Input type='text' placeholder='Search for tag ...' onChange={(e)=>{filter(e.target.value);}}/>
+	// 					<NewTag trigger={setTrigger}/>
+	// 				</HStack>
+	// 				<VStack p={2}>
+	// 					{results.map((t,index)=>(
+	// 						<CustomNavLink key={index} to={`./${t.id}`} w='100%'>
+	// 							{t.name}
+	// 						</CustomNavLink>
+
+	// 					))}
+				
+// 				</VStack>
+// 			</Box>
+// 			<Box w='70%' minH='100%' bg='white'>
+// 				<Outlet />
+// 			</Box> 
+// 		</Flex>
+// 	);
 };
 
 export default CreateProject;
-
