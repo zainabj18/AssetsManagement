a6d35a04a90e31aeee448bbada21350dc704e5c7
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-10 18:44:53 +0000

    Relationships are now shown

--- frontend/src/components/RadialGraph.js
+++ frontend/src/components/RadialGraph.js
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 const RadialGraph = ({ data }) => {
 
 	const size = 400;
@@ -20,6 +22,29 @@ const RadialGraph = ({ data }) => {
 		return 'translate(' + x + 'px, ' + y + 'px)';
 	};
 
+	const setCurve = (line) => {
+		let offset = 1;
+		let offx = (((offset) * center) + ((line.x2 + line.x1) * 0.5)) / (offset + 1);
+		let offy = (((offset) * center) + ((line.y2 + line.y1) * 0.5)) / (offset + 1);
+		return 'M' + line.x1 + ' ' + line.y1 + ' Q ' + offx + ' ' + offy + ' ' + line.x2 + ' ' + line.y2;
+	};
+
+	const calcPoints = () => {
+		let points = {};
+		data.forEach((dataPoint, index) => {
+			points[dataPoint.from] = index;
+		});
+		let coords = [];
+		data.forEach((dataPoint, indexFrom) => {
+			dataPoint.to.forEach((to) => {
+				coords.push({ x1: getX(indexFrom), y1: getY(indexFrom), x2: getX(points[to]), y2: getY(points[to]) });
+			});
+		});
+		return coords;
+	};
+
+	let lines = calcPoints();
+
 	return (
 		<div style={{ display: 'inline-block', height: size + 'px', width: size + 'px', backgroundColor: 'yellow' }}>
 			{data.map((dataPoint, index) => {
@@ -35,6 +60,26 @@ const RadialGraph = ({ data }) => {
 					</h1 >
 				);
 			})}
+			<svg
+				style={{
+					position: 'absolute',
+					height: size + 'px',
+					width: size + 'px'
+				}}
+			>
+				{lines.map((line, index) => {
+					return (
+						<path
+							key={index}
+							d={setCurve(line)}
+							stroke='red'
+							strokeWidth='1'
+							strokeLinecap="round"
+							fill="transparent">
+						</path>
+					);
+				})}
+			</svg>
 		</div>
 	);
 };
