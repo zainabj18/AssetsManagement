d64035c75d65bb98f1f612bd8a167e1e2420de70
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-13 22:25:50 +0000

    Fixed linting warnings

--- frontend/src/components/Graph/Line.js
+++ frontend/src/components/Graph/Line.js
@@ -9,9 +9,9 @@ export default class Line {
 
 	equals(line) {
 		return (
-			this.x1 === line.x1 && this.y1 === line.y1 && this.x2 === line.x2 && this.y2 == line.y2
+			(this.x1 === line.x1 && this.y1 === line.y1 && this.x2 === line.x2 && this.y2 === line.y2)
 			||
-			this.x1 === line.x2 && this.y1 === line.y2 && this.x2 === line.x1 && this.y2 === line.y1
+			(this.x1 === line.x2 && this.y1 === line.y2 && this.x2 === line.x1 && this.y2 === line.y1)
 		);
 	}
 }
