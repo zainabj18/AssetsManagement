c83304a6efcb6b632c6e4008eae3c39fc59cae68
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-15 22:36:05 +0000

    Styled comments

--- frontend/src/routes/assets/Comments.js
+++ frontend/src/routes/assets/Comments.js
@@ -1,20 +1,46 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Box, VStack,Text, Flex, StackDivider, HStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Link, NavLink, useParams } from 'react-router-dom';
 import { fetchComments } from '../../api';
 import useAuth from '../../hooks/useAuth';
+import {TimeIcon} from '@chakra-ui/icons';
 const Comments = () => {
-
+	const [comments, setComments] = useState([]);
 	const { id } = useParams();
 	const {user} = useAuth();
 
 	useEffect(() => {
-		fetchComments(id).then((data)=>{
+		fetchComments(id).then((res)=>{
 			console.log(user.userID);
-			console.log(data);
+			console.log(res.data);
+			setComments(res.data);
+
 		});
 	}, [id,user]);
     
-	return (<p>hello</p>);
+	return (
+		<VStack display="flex" flexDirection='column' bg="tomato" spacing={4} padding={4}>
+			{comments.map((comment, index) => {return (
+
+				<Box 
+					key={index}
+					backgroundColor="white"
+					borderRadius='md' 
+					padding={2}
+					maxWidth="80%"
+					alignSelf={index >1?'flex-start':'flex-end'}>
+					<Link to={`/profile/${comment.accountID}`} as={NavLink}><Text as='b'>{comment.username}</Text></Link>
+
+					<Text>{comment.comment}</Text>
+					<HStack>
+						<TimeIcon/>
+						<Text as="i" fontSize='xs'>{new Date(comment.datetime).toLocaleString()}</Text>
+					</HStack>
+			  	</Box>	
+			);})}
+	  </VStack>
+	);
+	
 };
  
 export default Comments;
