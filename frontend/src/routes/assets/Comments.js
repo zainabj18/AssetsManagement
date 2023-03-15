3d6d679da8a67654d8f5cd5d4aa5bc1bf2b19d3b
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-15 22:48:13 +0000

    Added scroll to botton of comments on load

--- frontend/src/routes/assets/Comments.js
+++ frontend/src/routes/assets/Comments.js
@@ -1,15 +1,22 @@
 import { Box, VStack,Text, Flex, StackDivider, HStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link, NavLink, useParams } from 'react-router-dom';
 import { fetchComments } from '../../api';
 import useAuth from '../../hooks/useAuth';
 import {TimeIcon} from '@chakra-ui/icons';
+
+const ScrollToBottom = () => {
+	const endRef = useRef();
+	useEffect(() => endRef.current.scrollIntoView({ behavior: 'smooth' }));
+	return <div ref={endRef} />;
+};
 const Comments = () => {
 	const [comments, setComments] = useState([]);
 	const { id } = useParams();
 	const {user} = useAuth();
 
 	useEffect(() => {
+
 		fetchComments(id).then((res)=>{
 			console.log(user.userID);
 			console.log(res.data);
@@ -19,9 +26,8 @@ const Comments = () => {
 	}, [id,user]);
     
 	return (
-		<VStack display="flex" flexDirection='column' bg="tomato" spacing={4} padding={4}>
+		<VStack display="flex" flexDirection='column' bg="tomato" spacing={4} padding={4} overflowY='scroll' maxH="70vh">
 			{comments.map((comment, index) => {return (
-
 				<Box 
 					key={index}
 					backgroundColor="white"
@@ -38,6 +44,7 @@ const Comments = () => {
 					</HStack>
 			  	</Box>	
 			);})}
+			<ScrollToBottom />
 	  </VStack>
 	);
 	
