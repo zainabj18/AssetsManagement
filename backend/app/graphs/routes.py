df5c3471e3ae505f06f43fee1a3fea0288c03553
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-16 15:28:12 +0000

    The data for the graph is now implimented for the backend.

--- backend/app/graphs/routes.py
+++ backend/app/graphs/routes.py
@@ -5,4 +5,27 @@ bp = Blueprint("graphs", __name__, url_prefix="/graph")
 
 @bp.route("/assets", methods=["GET"])
 def get_assets():
-    return {}, 200
+    query = """
+    SELECT asset_id, name
+    FROM assets;
+    """
+    sub_query = """
+    SELECT to_asset_id
+    FROM assets_in_assets
+    WHERE from_asset_id = %(from)s;
+    """
+    db = get_db()
+    with db.connection() as conn:
+        res = conn.execute(query)
+        assets = res.fetchall()
+        for i, tup in enumerate(assets):
+            assets[i] = {"id": tup[0], "name": tup[1]}
+        data = []
+        for asset in assets:
+            asset_id = asset["id"]
+            res = conn.execute(sub_query, {"from" : asset_id})
+            to = res.fetchall()
+            for i, item in enumerate(to):
+                to[i] = item[0]
+            data.append({"from": asset_id ,"to": to})
+    return {"data": {"points": assets, "joins": data}}, 200
--- backend/tests/integration/test_graph.py
+++ backend/tests/integration/test_graph.py
@@ -1,5 +1,4 @@
-from psycopg.rows import dict_row
-
+# Tests that all assets are reutrned along with the assets they link to
 def test_allAssets(client, db_conn):
     create_type_query = """
     INSERT INTO types(type_name)
@@ -13,11 +12,40 @@ def test_allAssets(client, db_conn):
     INSERT INTO assets(name, link, version_id, description)
     VALUES(%(name)s, 'example.com', 1, 'I am a description');
     """
-    with db_conn.cursor(row_factory=dict_row) as cur:
-        cur.execute(create_type_query)
-        cur.execute(create_type_version_query)
+    link_assets_query = """
+    INSERT INTO assets_in_assets(from_asset_id, to_asset_id)
+    VALUES(%(from)s, %(to)s);
+    """
+    with db_conn as conn:
+        conn.execute(create_type_query)
+        conn.execute(create_type_version_query)
         for i in range(0, 5):
-            string = "asset" + str(i)
-            cur.execute(create_asset_query, {"name": string})
+            string = "asset" + str(i+1)
+            conn.execute(create_asset_query, {"name": string})
+        conn.execute(link_assets_query, {"from": 1, "to": 2})
+        conn.execute(link_assets_query, {"from": 1, "to": 3})
+        conn.execute(link_assets_query, {"from": 2, "to": 1})
+        conn.execute(link_assets_query, {"from": 2, "to": 4})
+        conn.execute(link_assets_query, {"from": 3, "to": 2})
+        conn.execute(link_assets_query, {"from": 3, "to": 4})
+        conn.execute(link_assets_query, {"from": 3, "to": 5})
+        conn.execute(link_assets_query, {"from": 4, "to": 1})
+        conn.execute(link_assets_query, {"from": 4, "to": 5})
+        conn.execute(link_assets_query, {"from": 5, "to": 1})
     res = client.get("/api/v1/graph/assets")
     assert res.status_code == 200
+    data = res.json["data"]
+    assert data["points"] == [
+        {'id': 1, 'name': 'asset1'},
+        {'id': 2, 'name': 'asset2'},
+        {'id': 3, 'name': 'asset3'},
+        {'id': 4, 'name': 'asset4'},
+        {'id': 5, 'name': 'asset5'}
+    ]
+    assert data["joins"] == [
+        {'from': 1, 'to': [2, 3]},
+        {'from': 2, 'to': [1, 4]},
+        {'from': 3, 'to': [2, 4, 5]},
+        {'from': 4, 'to': [1, 5]},
+        {'from': 5, 'to': [1]}
+    ]
