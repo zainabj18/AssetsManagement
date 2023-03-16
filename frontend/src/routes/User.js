5b672f34dbfaa8a8f71aa5d1cc6773de71406e5e
Zayn Rashid <rangedbear@gmail.com>
2023-03-16 21:37:09 +0000

    Implemented if statement within saveUser function that sends an error if password and confirm password aren't the same.

--- frontend/src/routes/User.js
+++ frontend/src/routes/User.js
@@ -64,6 +64,11 @@ const User = () => {
 		const isFormValid = inputFields.every((field) => {
 		  return field.name !== '' && field.surname !== '' && field.username !== '' && field.password !== '' && field.confirm_password !== '';
 		});
+
+		if (password !== confirm_password) {
+			console.error('Passwords do not match');
+			return;
+		  }
 	  
 		if (isFormValid) {
 			const userData = {
