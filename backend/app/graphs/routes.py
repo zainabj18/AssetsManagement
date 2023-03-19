a7847391d1bd8910e3bfb6d365ad5579f1dfbdc4
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-19 12:27:08 +0000

    All types can now be gotten

--- backend/app/graphs/routes.py
+++ backend/app/graphs/routes.py
@@ -83,3 +83,38 @@ def get_asset(id):
             points.append({"id": key, "name": res.fetchone()[0]})
     print(data)
     return {"data": {"points": points, "joins": data}}, 200
+
+
+@bp.route("/types", methods=["GET"])
+def get_types():
+    query = """
+    SELECT version_id, type_name
+    FROM type_version AS v1
+    INNER JOIN types as t ON v1.type_id = t.type_id
+    WHERE version_number = 
+    (
+        SELECT MAX(v2.version_number)
+        FROM type_version as v2
+        WHERE v1.type_id = v2.type_id
+    )
+    GROUP BY v1.type_id, version_id, type_name;
+    """
+    sub_query = """
+    SELECT type_version_to
+    FROM type_version_link
+    WHERE type_version_from = %(from)s;
+    """
+    db = get_db()
+    with db.connection() as conn:
+        res = conn.execute(query)
+        types = res.fetchall()
+        for i, tup in enumerate(types):
+            types[i] = {"id": tup[0], "name": tup[1]}
+        data = []
+        for type in types:
+            version_id = type["id"]
+            res = conn.execute(sub_query, {"from": version_id})
+            to = res.fetchall()
+            to = remove_end(to)
+            data.append({"from": version_id, "to": to})
+    return {"data": {"points": types, "joins": data}}, 200
--- backend/tests/integration/test_graph.py
+++ backend/tests/integration/test_graph.py
@@ -20,6 +20,22 @@ def populate_db_assets(db_conn, count):
             conn.execute(create_asset_query, {"name": string})
 
 
+# Poplates db with 'count' number of types
+def populate_db_types(db_conn, count):
+    create_type_query = """
+    INSERT INTO types(type_name)
+    VALUES(%(name)s);
+    """
+    create_type_version_query = """
+    INSERT INTO type_version(version_number, type_id)
+    VALUES(1, %(type)s);
+    """
+    with db_conn as conn:
+        for i in range(0, count):
+            conn.execute(create_type_query, {"name": ("type" + str(i+1))})
+            conn.execute(create_type_version_query, {"type": i + 1})
+
+
 # Tests that all assets are reutrned along with the assets they link to
 def test_all_assets(client, db_conn):
     link_assets_query = """
@@ -93,3 +109,40 @@ def test_wrong_asset_path(client):
     data = res.json
     assert data["data"] == {"points": [], "joins": []}
     assert data["msg"] == "Given id was not an int."
+
+
+# Tests that all types are reutrned along with the types they link to
+def test_all_types(client, db_conn):
+    link_types_query = """
+    INSERT INTO type_version_link(type_version_from, type_version_to)
+    VALUES(%(from)s, %(to)s);
+    """
+    populate_db_types(db_conn, 5)
+    with db_conn as conn:
+        conn.execute(link_types_query, {"from": 1, "to": 2})
+        conn.execute(link_types_query, {"from": 1, "to": 3})
+        conn.execute(link_types_query, {"from": 2, "to": 1})
+        conn.execute(link_types_query, {"from": 2, "to": 4})
+        conn.execute(link_types_query, {"from": 3, "to": 2})
+        conn.execute(link_types_query, {"from": 3, "to": 4})
+        conn.execute(link_types_query, {"from": 3, "to": 5})
+        conn.execute(link_types_query, {"from": 4, "to": 1})
+        conn.execute(link_types_query, {"from": 4, "to": 5})
+        conn.execute(link_types_query, {"from": 5, "to": 1})
+    res = client.get("/api/v1/graph/types")
+    assert res.status_code == 200
+    data = res.json["data"]
+    assert data["points"] == [
+        {'id': 1, 'name': 'type1'},
+        {'id': 2, 'name': 'type2'},
+        {'id': 3, 'name': 'type3'},
+        {'id': 4, 'name': 'type4'},
+        {'id': 5, 'name': 'type5'}
+    ]
+    assert data["joins"] == [
+        {'from': 1, 'to': [2, 3]},
+        {'from': 2, 'to': [1, 4]},
+        {'from': 3, 'to': [2, 4, 5]},
+        {'from': 4, 'to': [1, 5]},
+        {'from': 5, 'to': [1]}
+    ]
