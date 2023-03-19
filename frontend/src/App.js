96fe719573d53358ac22996fe7ae9833a9ab6f3c
Dmani Barnett <dmani.barnett@gmail.com>
2023-03-19 09:56:18 +0000

    Added error boundary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,51 +21,54 @@ import ProjectViewer from './routes/ProjectViewer';
 import TypeEditor from './routes/TypeEditor';
 import Logs from './routes/Logs';
 import Comments from './routes/assets/Comments';
+import {ErrorBoundary} from 'react-error-boundary';
 import { fetchRelatedClassification, fetchRelatedFrom, fetchRelatedProjects, fetchRelatedTags, fetchRelatedTo, fetchRelatedType } from './api';
+import ErrorFallback from './routes/ErrorFallback';
+
 function App() {
-
 	return (
-		<AuthProvider>
-			<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/" element={<Layout />}>
-					<Route path="assets/" element={<SubLayout name="Assets"/>}>
-						<Route index element={<AssetsOverview />} />
-						<Route path="new" element={<AssetViewer />} />
-						<Route path=":id" element={<AssetOverview />}>
-							<Route index element={<AssetViewer />} />	
-							<Route path="comments" element={<Comments />} />	
-							<Route path="logs" element={<AssetLogs />} />
-							<Route path="type" element={<RelatedToTable relatedFunc={fetchRelatedType}/>} />	
-							<Route path="classification" element={<RelatedToTable relatedFunc={fetchRelatedClassification}/>} />
-							<Route path="tags" element={<RelatedToTable relatedFunc={fetchRelatedTags}/>} />
-							<Route path="projects" element={<RelatedToTable relatedFunc={fetchRelatedProjects}/>} />
-							<Route path="outgoing" element={<RelatedToTable relatedFunc={fetchRelatedFrom}/>} />
-							<Route path="incomming" element={<RelatedToTable relatedFunc={fetchRelatedTo}/>} />
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<AuthProvider>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/" element={<Layout />}>
+						<Route path="assets/" element={<SubLayout name="Assets"/>}>
+							<Route index element={<AssetsOverview />} />
+							<Route path="new" element={<AssetViewer />} />
+							<Route path=":id" element={<AssetOverview />}>
+								<Route index element={<AssetViewer />} />	
+								<Route path="comments" element={<Comments />} />	
+								<Route path="logs" element={<AssetLogs />} />
+								<Route path="type" element={<RelatedToTable relatedFunc={fetchRelatedType}/>} />	
+								<Route path="classification" element={<RelatedToTable relatedFunc={fetchRelatedClassification}/>} />
+								<Route path="tags" element={<RelatedToTable relatedFunc={fetchRelatedTags}/>} />
+								<Route path="projects" element={<RelatedToTable relatedFunc={fetchRelatedProjects}/>} />
+								<Route path="outgoing" element={<RelatedToTable relatedFunc={fetchRelatedFrom}/>} />
+								<Route path="incomming" element={<RelatedToTable relatedFunc={fetchRelatedTo}/>} />
+							</Route>
 						</Route>
-					</Route>
-					<Route path="projects/" element={<SubLayout name="Projects"/>}>
-						<Route index element={<ProjectViewer/>} />
-						<Route path="new" element={<CreateProject />} />
-					</Route>
+						<Route path="projects/" element={<SubLayout name="Projects"/>}>
+							<Route index element={<ProjectViewer/>} />
+							<Route path="new" element={<CreateProject />} />
+						</Route>
 
-					<Route path="tags/" element={<Tags />}>
-						<Route path=":id" element={<TagViewer/>} />	
+						<Route path="tags/" element={<Tags />}>
+							<Route path=":id" element={<TagViewer/>} />	
+						</Route>
+						<Route path="type/" element={<SubLayout name="Types"/>}>
+							<Route index element={<TypeViewer />} />
+							<Route path="adder" element={<TypeAdder />} />
+							<Route path="attributes" element={<AttributeViewer />} />
+							<Route path=":id" element={<TypeEditor />} />
+						</Route>
+						<Route path="accounts" element={<AdminManager />} />
+						<Route path="user" element={<User />} />
+						<Route path="logs" element={<Logs />} />
 					</Route>
-					<Route path="type/" element={<SubLayout name="Types"/>}>
-						<Route index element={<TypeViewer />} />
-						<Route path="adder" element={<TypeAdder />} />
-						<Route path="attributes" element={<AttributeViewer />} />
-						<Route path=":id" element={<TypeEditor />} />
-					</Route>
-					<Route path="accounts" element={<AdminManager />} />
-					<Route path="user" element={<User />} />
-					<Route path="logs" element={<Logs />} />
-				</Route>
-				<Route path="*" element={<NoMatch />} />
-			</Routes>
-		</AuthProvider>
-
+					<Route path="*" element={<NoMatch />} />
+				</Routes>
+			</AuthProvider>
+		</ErrorBoundary>
 	);
 }
 
