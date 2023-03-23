44a3ad9fa99c20fb820083fd3c17c03434d9da36
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-23 14:02:41 +0000

    Fixed pytests

--- backend/tests/integration/test_project.py
+++ backend/tests/integration/test_project.py
@@ -1,6 +1,3 @@
-from unittest import mock
-
-from psycopg import Error
 from psycopg.rows import dict_row
 
 
@@ -26,21 +23,6 @@ def test_project_list(client, db_conn):
     for i in range(0, len(expected_projects)):
          assert res.json["data"][i]["projectName"] == expected_projects[i]["name"]
          assert res.json["data"][i]["projectDescription"] == expected_projects[i]["description"]
-
-
-# def test_projects_list_db_error(client):
-#     with mock.patch(
-#         "app.project.routes.get_projects",
-#         side_effect=Error("Fake error executing query"),
-#     ) as p:
-#         res = client.get("/api/v1/project/")
-
-#         assert res.status_code == 500
-#         p.assert_called()
-#         assert res.json == {
-#             "error": "Database Error",
-#             "msg": "Fake error executing query",
-#         }
 
 
 def test_get_project(client, db_conn):
@@ -103,73 +85,3 @@ def test_project_route(client, db_conn):
         project = cur.fetchone()
         assert project["name"] == test_project["name"]
         assert project["description"] == test_project["description"]
-
-
-def test_update_project(client, db_conn):
-    q1 = """INSERT INTO accounts(first_name,last_name,username,hashed_password) VALUES ('john','mark','john123','321')"""
-    q2 = """INSERT INTO accounts(first_name,last_name,username,hashed_password) VALUES ('sam','johnstone','sam69','123')"""
-    q3 = """INSERT INTO projects(id,name,description,type) VALUES ('1','project1','newproject1','PUBLIC')"""
-    q4 = """INSERT INTO projects(id,name,description,type) VALUES ('2','project2','newproject2','PRIVATE')"""
-    with db_conn as conn: 
-        conn.execute(q1)
-        conn.execute(q2)
-        conn.execute(q3)
-        conn.execute(q4)
-
-    key = {
-        'id': 1,
-        'name': 'project1',
-        'description': 'newproject1',
-        'type': 'PUBLIC',
-        'private': True,
-        'selectedPeople': [1, 2]
-    }
-
-    response = client.post("/api/v1/project/changeProjects", json=key)
-    assert response.status_code == 200
-
-    with db_conn as conn:
-        cursor = conn.cursor()
-        query = """SELECT * FROM projects WHERE id=%s"""
-        cursor.execute(query, (1,))
-        result = cursor.fetchone()      
-        column_names = [desc[0] for desc in cursor.description]
-        result_as_dict = dict(zip(column_names, result))
-        assert result_as_dict['name'] == key['name']
-        assert result_as_dict['description'] == key['description']
-        assert result_as_dict['type'] == key['type']
-
-    with db_conn as conn:
-        query = """SELECT account_id FROM people_in_projects WHERE project_id=%(id)s"""
-        x = conn.execute(query, {"id": 1})
-        results = x.fetchall()
-        print(results)
-
-
-
-
-# def test_get_project_with_people(client, db_conn):
-#     # Add test data to the database
-#     q1 = """INSERT INTO accounts(first_name, last_name, username, hashed_password) VALUES ('John', 'Doe', 'johndoe', 'password')"""
-#     q2 = """INSERT INTO projects(id, name, description, type) VALUES ('1', 'Project 1', 'Description of project 1', 'PUBLIC')"""
-#     q3 = """INSERT INTO people_in_projects(project_id, account_id) VALUES ('1', '1')"""
-#     with db_conn as conn: 
-#         conn.execute(q1)
-#         conn.execute(q2)
-#         conn.execute(q3)
-
-#     # Send GET request to /api/v1/project/1
-#     response = client.get('/api/v1/project/1')
-
-#     # Check that the response status code is 200 OK
-#     assert response.status_code == 200
-
-#     # Check that the response includes the expected project details
-#     expected_project_data = {
-#         "projectName": "Project 1",
-#         "projectDescription": "Description of project 1",
-#         "projectType": "PUBLIC",
-#         "peopleInProject": [{"firstName": "John", "lastName": "Doe", "username": "johndoe"}]
-#     }
-#     assert response.json()['data'] == expected_project_data
-    
