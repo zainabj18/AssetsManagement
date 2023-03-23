2784efc03f2aa196bf14855c4e2525194a070f64
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-23 17:26:59 +0000

    Ajusted asset overview to accomodate for table pages.

--- frontend/src/routes/assets/AssetsOverview.js
+++ frontend/src/routes/assets/AssetsOverview.js
@@ -31,7 +31,7 @@ const AssetsOverview = () => {
 
 	return (<VStack width={'100vw'} height={'80vh'} background='white' justifyContent={'space-between'} overflow='hidden' flexDirection={'row-reverse'} display='flex' paddingX={5} paddingY={5} rounded='2xl' boxShadow='0 3px 6px #00000029'>
 		<div style={{ width: '75vw', flexDirection: 'column', justifyContent: 'center', display: 'flex' }} >
-			<div style={{ width: '75vw', overflow: 'hidden', height: '30vw', overflowY: 'auto' }}>
+			<div style={{ width: '75vw', overflow: 'hidden' }}>
 				<AssetTable assets={results} setSelectedAssets={null} preSelIDs={[]} />
 			</div>
 			<Button onClick={() => naviagte('./new')} width='50%' paddingY={6} alignSelf='center'>Create Asset</Button>
