ed9a8a0ff08139fb7c2022122cefe882c23f8336
zkac326 <robert.dorling.2021@live.rhul.ac.uk>
2023-03-23 17:46:19 +0000

    Added padding to type and attribute tables.

--- frontend/src/routes/AttributeViewer.js
+++ frontend/src/routes/AttributeViewer.js
@@ -1,6 +1,6 @@
 import {
 	Button, useBoolean,
-	TableContainer, Table, TableCaption, Thead, Tbody, Th, Tr, Td, VStack
+	TableContainer, Table, TableCaption, Thead, Tbody, Th, Tr, Td, VStack, Box
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import useAuth from '../hooks/useAuth';
@@ -35,37 +35,39 @@ const AttributeViewer = () => {
 
 	return (
 		<VStack height={'85vh'}>
-			<div style={{ height: '72vh', overflow: 'scroll' }}>
-				<Table>
-					{/* <TableCaption placement='top'>Attribute Viewer</TableCaption> */}
-					<Thead zIndex={999}>
-						<Tr>
-							<Th>Name</Th>
-							<Th>Type</Th>
-							<Th>Is Optional</Th>
-							<Th>Delete</Th>
-						</Tr>
-					</Thead>
-					<Tbody zIndex={-99}>
-						{attributes.map((attributes) => {
-							return (
-								<Tr key={attributes.attributeID} zIndex={-1}>
-									<Td>{attributes.attributeName}</Td>
-									<Td>{attributes.attributeType}</Td>
-									<Td>{attributes.validation.isOptional.toString()}</Td>
-									<Td>
-										{
-											(user && user.userRole === 'ADMIN') &&
-											<div style={{ background: '#0a2861', padding: 5, borderRadius: 5, color: '#fff' }} onClick={() => deleteThis(attributes)}>Delete</div >
-										}
-									</Td>
-								</Tr>
+			<Box padding='4' backgroundColor='white' rounded='15'>
+				<Box style={{ height: '72vh', overflow: 'auto' }}>
+					<Table>
+						{/* <TableCaption placement='top'>Attribute Viewer</TableCaption> */}
+						<Thead zIndex={999}>
+							<Tr>
+								<Th>Name</Th>
+								<Th>Type</Th>
+								<Th>Is Optional</Th>
+								<Th>Delete</Th>
+							</Tr>
+						</Thead>
+						<Tbody zIndex={-99}>
+							{attributes.map((attributes) => {
+								return (
+									<Tr key={attributes.attributeID} zIndex={-1}>
+										<Td>{attributes.attributeName}</Td>
+										<Td>{attributes.attributeType}</Td>
+										<Td>{attributes.validation.isOptional.toString()}</Td>
+										<Td>
+											{
+												(user && user.userRole === 'ADMIN') &&
+												<div style={{ background: '#0a2861', padding: 5, borderRadius: 5, color: '#fff' }} onClick={() => deleteThis(attributes)}>Delete</div >
+											}
+										</Td>
+									</Tr>
 
-							);
-						})}
-					</Tbody>
-				</Table>
-			</div>
+								);
+							})}
+						</Tbody>
+					</Table>
+				</Box>
+			</Box>
 			<AttributeModal showModalButtonText='New' load_allAttributes_setter={set_toggle} />
 		</VStack>
 	);
